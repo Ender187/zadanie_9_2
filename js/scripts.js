cad0a345aeeec25245b20e83df4334e1d06869bf
Maciej <m.hiltawski@hotmail.com>
2017-04-29 23:19:31 +0200

    Delete scripts.js

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-var text: 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var animal = 'Zielone słonie';
-var animalUpperCase = animal.toUpperCase();
-var textChange = text.replace('Papugi', animalUpperCase);
-console.log(text.length/2);
